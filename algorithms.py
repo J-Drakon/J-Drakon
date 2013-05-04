2cc3a6b6405914d2fefc16e3932872a07b01c122
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2013-05-04 15:40:39 +0530

    add helper function

---
+++ algorithms.py
@@ -0,0 +1,21 @@
+"""Module updated - revision 3"""
+
+def transform_data_0(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v3
