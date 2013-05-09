43ffa03bb340d80c6b5e90709047735917f45e2c
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2013-05-09 16:12:26 +0530

    update notes (in module)

--- solutions.py
+++ solutions.py
@@ -1,14 +1,20 @@
 """Module updated - revision 4"""
 
-def compute_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def transform_data_1(input_val):
+def check_data_1(input_val):
     result = input_val
     return result
 
-def validate_data_2(input_val):
+def transform_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def handle_data_3(input_val):
     result = input_val
     if not result:
         return None
