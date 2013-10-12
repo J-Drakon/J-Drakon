6cc47beed90a3910b372255cb13aeccab697307b
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2013-10-12 11:28:28 +0530

    add linked list impl

--- solutions.py
+++ solutions.py
@@ -1,23 +1,19 @@
-"""Module updated - revision 4"""
+"""Module updated - revision 7"""
 
-def handle_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     return result
 
-def check_data_1(input_val):
+def handle_data_1(input_val):
     result = input_val
     return result
 
-def transform_data_2(input_val):
+def handle_data_2(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def handle_data_3(input_val):
+def compute_data_3(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v4
+# v7
