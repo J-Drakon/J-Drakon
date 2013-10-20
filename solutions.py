9cd0fb3b907721892c14b77caa1122c70203a577
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2013-10-20 11:51:12 +0530

    practice recursion (after review)

--- solutions.py
+++ solutions.py
@@ -1,27 +1,23 @@
-"""Module updated - revision 8"""
+"""Module updated - revision 10"""
 
-def parse_data_0(input_val):
+def check_data_0(input_val):
+    result = input_val
+    return result
+
+def fetch_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_1(input_val):
+def check_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_2(input_val):
+def handle_data_3(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def validate_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v8
+# v10
