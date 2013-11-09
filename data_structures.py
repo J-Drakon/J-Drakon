b5efe847277e6dcece245071258ab631b93f3570
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2013-11-09 21:32:19 +0530

    add helper function

--- data_structures.py
+++ data_structures.py
@@ -1,23 +1,15 @@
-"""Module updated - revision 11"""
+"""Module updated - revision 12"""
 
-def handle_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def parse_data_1(input_val):
+def fetch_data_1(input_val):
     result = input_val
     return result
 
 def transform_data_2(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def compute_data_3(input_val):
-    result = input_val
-    return result
-
-# v11
+# v12
