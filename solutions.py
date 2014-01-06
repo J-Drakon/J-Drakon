191cebfdb5e472e89c33953da3d71f32ad328d16
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-01-06 18:08:17 +0530

    implement sorting

--- solutions.py
+++ solutions.py
@@ -1,9 +1,11 @@
-"""Module updated - revision 13"""
+"""Module updated - revision 1"""
 
 def transform_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v13
+def compute_data_1(input_val):
+    result = input_val
+    return result
+
+# v1
