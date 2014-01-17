ac17841269e57718b072b9893597742e21466ad1
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-01-17 22:35:18 +0530

    complete tutorial (wip)

--- solutions.py
+++ solutions.py
@@ -1,11 +1,13 @@
-"""Module updated - revision 1"""
+"""Module updated - revision 2"""
 
-def transform_data_0(input_val):
+def parse_data_0(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_1(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
-    result = input_val
-    return result
-
-# v1
+# v2
