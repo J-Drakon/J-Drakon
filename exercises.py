dba97a4d0eaed57b6bd3e63e08f2747976632ffd
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-01-25 13:04:36 +0530

    add helper function (in module)

--- exercises.py
+++ exercises.py
@@ -1,7 +1,9 @@
-"""Module updated - revision 8"""
+"""Module updated - revision 3"""
 
 def transform_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v8
+# v3
