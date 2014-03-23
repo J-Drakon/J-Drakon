b78aace8e5b664d5de18b5a60cf3f609ccda4635
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-03-23 19:17:46 +0530

    fix off by one (wip)

--- solutions.py
+++ solutions.py
@@ -1,23 +1,7 @@
-"""Module updated - revision 4"""
+"""Module updated - revision 5"""
 
 def compute_data_0(input_val):
     result = input_val
     return result
 
-def parse_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def parse_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def check_data_3(input_val):
-    result = input_val
-    return result
-
-# v4
+# v5
