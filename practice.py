e356d41dec02d59015b12bca520f0664e93c216f
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-04-04 07:48:26 +0530

    implement stack

--- practice.py
+++ practice.py
@@ -1,13 +1,7 @@
-"""Module updated - revision 13"""
+"""Module updated - revision 6"""
 
-def check_data_0(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def check_data_1(input_val):
+def validate_data_0(input_val):
     result = input_val
     return result
 
-# v13
+# v6
