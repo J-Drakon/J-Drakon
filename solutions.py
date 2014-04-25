746ca9ee5a5fcd90a05757dbfd064602375cd78a
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-04-25 16:03:39 +0530

    add solution

--- solutions.py
+++ solutions.py
@@ -1,7 +1,23 @@
-"""Module updated - revision 5"""
+"""Module updated - revision 9"""
 
 def compute_data_0(input_val):
     result = input_val
     return result
 
-# v5
+def transform_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def parse_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def handle_data_3(input_val):
+    result = input_val
+    return result
+
+# v9
