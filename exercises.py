253762cf010e6567a52b1d5a4cae1972a26dd9f0
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-05-05 12:29:17 +0530

    add linked list impl

--- exercises.py
+++ exercises.py
@@ -1,17 +1,13 @@
-"""Module updated - revision 10"""
+"""Module updated - revision 11"""
 
-def parse_data_0(input_val):
-    result = input_val
-    return result
-
-def check_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def transform_data_2(input_val):
+def fetch_data_1(input_val):
     result = input_val
     return result
 
-# v10
+# v11
