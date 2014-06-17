a037316d4c45aa96e1845d36b2bad1abb7c2cfb4
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-06-17 11:37:27 +0530

    update notes

--- exercises.py
+++ exercises.py
@@ -1,25 +1,23 @@
-"""Module updated - revision 13"""
+"""Module updated - revision 15"""
 
-def parse_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     return result
 
-def handle_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_2(input_val):
+def compute_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_3(input_val):
+def handle_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v13
+def transform_data_3(input_val):
+    result = input_val
+    return result
+
+# v15
