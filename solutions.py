af444ce316786673a77df0b502bb3eb6d4353b82
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-08-05 11:30:28 +0530

    optimize algorithm (after review)

--- solutions.py
+++ solutions.py
@@ -1,23 +1,19 @@
-"""Module updated - revision 9"""
+"""Module updated - revision 16"""
 
-def compute_data_0(input_val):
-    result = input_val
-    return result
-
-def transform_data_1(input_val):
+def process_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def parse_data_2(input_val):
+def check_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_3(input_val):
+def validate_data_2(input_val):
     result = input_val
     return result
 
-# v9
+# v16
