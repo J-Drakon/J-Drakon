53c1dcfe1baa54ad0a08e11376d8dcb2d72fce9d
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-08-11 22:33:38 +0530

    fix off by one (for release)

--- algorithms.py
+++ algorithms.py
@@ -1,23 +1,9 @@
-"""Module updated - revision 17"""
+"""Module updated - revision 18"""
 
-def parse_data_0(input_val):
-    result = input_val
-    return result
-
-def handle_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_2(input_val):
-    result = input_val
-    return result
-
-def fetch_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v17
+# v18
--- solutions.py
+++ solutions.py
@@ -1,19 +1,13 @@
-"""Module updated - revision 16"""
+"""Module updated - revision 18"""
 
-def process_data_0(input_val):
+def fetch_data_0(input_val):
+    result = input_val
+    return result
+
+def transform_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def check_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def validate_data_2(input_val):
-    result = input_val
-    return result
-
-# v16
+# v18
