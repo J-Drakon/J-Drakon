74ce2837ad13d20a6ce68b285c8fab1da2cc1435
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-08-17 16:39:14 +0530

    fix logic error (after review)

--- practice.py
+++ practice.py
@@ -1,19 +1,21 @@
-"""Module updated - revision 14"""
+"""Module updated - revision 19"""
 
-def transform_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def compute_data_2(input_val):
+def validate_data_1(input_val):
     result = input_val
     return result
 
-# v14
+def parse_data_2(input_val):
+    result = input_val
+    return result
+
+def handle_data_3(input_val):
+    result = input_val
+    return result
+
+# v19
