b7475532d0dfe3481898bbf8ee5e59dcb92d436a
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-08-26 18:09:09 +0530

    add binary search (for release)

--- solutions.py
+++ solutions.py
@@ -1,13 +1,21 @@
-"""Module updated - revision 18"""
+"""Module updated - revision 20"""
 
-def fetch_data_0(input_val):
-    result = input_val
-    return result
-
-def transform_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v18
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def fetch_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v20
