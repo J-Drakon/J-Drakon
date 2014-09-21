a0b685399314a06d782dc4aa6d7a09ae988ff0ae
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-09-21 22:40:55 +0530

    add data structures practice (wip)

--- algorithms.py
+++ algorithms.py
@@ -1,9 +1,23 @@
-"""Module updated - revision 18"""
+"""Module updated - revision 21"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v18
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def fetch_data_2(input_val):
+    result = input_val
+    return result
+
+def transform_data_3(input_val):
+    result = input_val
+    return result
+
+# v21
--- exercises.py
+++ exercises.py
@@ -1,23 +1,7 @@
-"""Module updated - revision 15"""
+"""Module updated - revision 21"""
 
-def validate_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def handle_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def transform_data_3(input_val):
-    result = input_val
-    return result
-
-# v15
+# v21
