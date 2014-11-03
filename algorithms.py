4fba586b30d2cfbd4400878701fd146949f29e02
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-11-03 16:19:05 +0530

    implement stack

--- algorithms.py
+++ algorithms.py
@@ -1,23 +1,13 @@
-"""Module updated - revision 21"""
+"""Module updated - revision 24"""
 
 def fetch_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def parse_data_1(input_val):
+def fetch_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def fetch_data_2(input_val):
-    result = input_val
-    return result
-
-def transform_data_3(input_val):
-    result = input_val
-    return result
-
-# v21
+# v24
--- data_structures.py
+++ data_structures.py
@@ -1,9 +1,23 @@
-"""Module updated - revision 22"""
+"""Module updated - revision 24"""
 
-def check_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v22
+def handle_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def parse_data_2(input_val):
+    result = input_val
+    return result
+
+def fetch_data_3(input_val):
+    result = input_val
+    return result
+
+# v24
