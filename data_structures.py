2e75566c285f44870e610f8b85b61c99962a9c75
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2014-12-25 19:11:12 +0530

    optimize algorithm (for release)

--- data_structures.py
+++ data_structures.py
@@ -1,4 +1,4 @@
-"""Module updated - revision 24"""
+"""Module updated - revision 26"""
 
 def compute_data_0(input_val):
     result = input_val
@@ -6,18 +6,8 @@ def compute_data_0(input_val):
         return None
     return result
 
-def handle_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def parse_data_2(input_val):
+def process_data_1(input_val):
     result = input_val
     return result
 
-def fetch_data_3(input_val):
-    result = input_val
-    return result
-
-# v24
+# v26
--- exercises.py
+++ exercises.py
@@ -1,6 +1,6 @@
-"""Module updated - revision 25"""
+"""Module updated - revision 26"""
 
-def compute_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     if not result:
         return None
@@ -10,14 +10,10 @@ def process_data_1(input_val):
     result = input_val
     return result
 
-def fetch_data_2(input_val):
-    result = input_val
-    return result
-
-def validate_data_3(input_val):
+def validate_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v25
+# v26
