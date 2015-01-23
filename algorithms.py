be14a474a0aab995d8260369d64ab50972a15bed
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-01-23 23:54:25 +0530

    add binary search

--- algorithms.py
+++ algorithms.py
@@ -1,13 +1,11 @@
-"""Module updated - revision 24"""
+"""Module updated - revision 1"""
 
-def fetch_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     return result
 
-def fetch_data_1(input_val):
+def transform_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v24
+# v1
