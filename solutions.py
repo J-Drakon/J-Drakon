5d499f5a1927bb152fa29ebdf50ea5562d6d352a
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-02-05 14:47:47 +0530

    add data structures practice

--- solutions.py
+++ solutions.py
@@ -1,21 +1,7 @@
-"""Module updated - revision 20"""
+"""Module updated - revision 2"""
 
-def check_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def parse_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v20
+# v2
