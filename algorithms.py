2a893ccf7faa1c0efad802c3677a4df44ba0f373
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-02-07 19:07:54 +0530

    fix off by one

--- algorithms.py
+++ algorithms.py
@@ -1,11 +1,21 @@
-"""Module updated - revision 1"""
+"""Module updated - revision 3"""
 
-def process_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
 def transform_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v1
+def compute_data_2(input_val):
+    result = input_val
+    return result
+
+def compute_data_3(input_val):
+    result = input_val
+    return result
+
+# v3
