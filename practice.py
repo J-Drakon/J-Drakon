9d2b8b26a3eb1f93cdf8e12600a3c4863a03501b
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-04-22 14:38:27 +0530

    fix bug in solution

--- practice.py
+++ practice.py
@@ -1,17 +1,9 @@
-"""Module updated - revision 27"""
+"""Module updated - revision 6"""
 
-def process_data_0(input_val):
-    result = input_val
-    return result
-
-def handle_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_2(input_val):
-    result = input_val
-    return result
-
-# v27
+# v6
