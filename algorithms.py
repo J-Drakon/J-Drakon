4a18fa9ff1344e8b7a188442bc74c865ffab3906
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-04-28 12:36:32 +0530

    fix bug in solution

--- algorithms.py
+++ algorithms.py
@@ -1,21 +1,7 @@
-"""Module updated - revision 3"""
+"""Module updated - revision 7"""
 
-def check_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def transform_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def compute_data_2(input_val):
-    result = input_val
-    return result
-
-def compute_data_3(input_val):
-    result = input_val
-    return result
-
-# v3
+# v7
