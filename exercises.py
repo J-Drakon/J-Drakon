0e30a2ea028665da6ccd3c8670d86d1a6161af48
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-05-02 16:04:08 +0530

    refactor code

--- exercises.py
+++ exercises.py
@@ -1,19 +1,7 @@
-"""Module updated - revision 26"""
+"""Module updated - revision 9"""
 
 def validate_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def process_data_1(input_val):
-    result = input_val
-    return result
-
-def validate_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v26
+# v9
