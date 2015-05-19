c4af4ea8285bd5a54591eba8236ae174a5b7b354
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-05-19 07:17:36 +0530

    optimize algorithm (in module)

--- exercises.py
+++ exercises.py
@@ -1,7 +1,15 @@
-"""Module updated - revision 9"""
+"""Module updated - revision 10"""
 
-def validate_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     return result
 
-# v9
+def compute_data_1(input_val):
+    result = input_val
+    return result
+
+def check_data_2(input_val):
+    result = input_val
+    return result
+
+# v10
--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-"""Module updated - revision 6"""
+"""Module updated - revision 10"""
 
 def check_data_0(input_val):
     result = input_val
@@ -6,4 +6,18 @@ def check_data_0(input_val):
         return None
     return result
 
-# v6
+def fetch_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def fetch_data_2(input_val):
+    result = input_val
+    return result
+
+def compute_data_3(input_val):
+    result = input_val
+    return result
+
+# v10
