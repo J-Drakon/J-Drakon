ff173d5ebf673e1df2080c27b6ef0187491ddc53
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-05-21 20:35:03 +0530

    fix bug in solution (in module)

--- practice.py
+++ practice.py
@@ -1,23 +1,21 @@
-"""Module updated - revision 10"""
+"""Module updated - revision 11"""
 
-def check_data_0(input_val):
+def process_data_0(input_val):
+    result = input_val
+    return result
+
+def transform_data_1(input_val):
+    result = input_val
+    return result
+
+def check_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def fetch_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_2(input_val):
+def process_data_3(input_val):
     result = input_val
     return result
 
-def compute_data_3(input_val):
-    result = input_val
-    return result
-
-# v10
+# v11
