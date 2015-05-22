a5e1ec30f0f7e68d0f7397d148d8fa907abe62b2
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-05-22 22:43:59 +0530

    update solution

--- exercises.py
+++ exercises.py
@@ -1,15 +1,13 @@
-"""Module updated - revision 10"""
+"""Module updated - revision 12"""
 
-def process_data_0(input_val):
+def check_data_0(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def validate_data_1(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
-    result = input_val
-    return result
-
-def check_data_2(input_val):
-    result = input_val
-    return result
-
-# v10
+# v12
