c3cce2012d6ad651e65b54cd4106f193cd18d279
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-06-10 18:31:45 +0530

    update solution (cleanup)

--- data_structures.py
+++ data_structures.py
@@ -1,15 +1,7 @@
-"""Module updated - revision 5"""
+"""Module updated - revision 13"""
 
-def handle_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def transform_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v5
+# v13
--- practice.py
+++ practice.py
@@ -1,21 +1,19 @@
-"""Module updated - revision 11"""
+"""Module updated - revision 13"""
 
-def process_data_0(input_val):
-    result = input_val
-    return result
-
-def transform_data_1(input_val):
-    result = input_val
-    return result
-
-def check_data_2(input_val):
+def parse_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def process_data_3(input_val):
+def validate_data_1(input_val):
     result = input_val
     return result
 
-# v11
+def validate_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v13
