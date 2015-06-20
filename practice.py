48abcd901b8f8ef4efee9f01c60103f57a1ceef6
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-06-20 15:39:02 +0530

    refactor code (after review)

--- practice.py
+++ practice.py
@@ -1,19 +1,25 @@
-"""Module updated - revision 13"""
+"""Module updated - revision 14"""
 
-def parse_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-def validate_data_2(input_val):
+def compute_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v13
+def handle_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def process_data_3(input_val):
+    result = input_val
+    return result
+
+# v14
