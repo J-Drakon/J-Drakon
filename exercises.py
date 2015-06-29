134be607bd99b7c0700ab8bcbaed289a897ff211
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-06-29 06:53:05 +0530

    add comments

--- exercises.py
+++ exercises.py
@@ -1,19 +1,17 @@
-"""Module updated - revision 15"""
+"""Module updated - revision 16"""
 
-def validate_data_0(input_val):
+def check_data_0(input_val):
+    result = input_val
+    return result
+
+def transform_data_1(input_val):
+    result = input_val
+    return result
+
+def check_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-def validate_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v15
+# v16
