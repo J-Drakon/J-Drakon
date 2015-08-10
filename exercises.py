e586449f557ba8fc160126aeacd138f2a20fc174
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-08-10 22:35:21 +0530

    add solution (after review)

--- exercises.py
+++ exercises.py
@@ -1,17 +1,13 @@
-"""Module updated - revision 16"""
+"""Module updated - revision 18"""
 
 def check_data_0(input_val):
-    result = input_val
-    return result
-
-def transform_data_1(input_val):
-    result = input_val
-    return result
-
-def check_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v16
+def process_data_1(input_val):
+    result = input_val
+    return result
+
+# v18
