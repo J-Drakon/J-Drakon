12d43938dec48dca23d256ac66e097de310b3a55
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-08-31 16:38:59 +0530

    fix edge case

--- exercises.py
+++ exercises.py
@@ -1,13 +1,23 @@
-"""Module updated - revision 18"""
+"""Module updated - revision 19"""
 
-def check_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def process_data_1(input_val):
+def check_data_1(input_val):
     result = input_val
     return result
 
-# v18
+def process_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def process_data_3(input_val):
+    result = input_val
+    return result
+
+# v19
--- practice.py
+++ practice.py
@@ -1,25 +1,21 @@
-"""Module updated - revision 14"""
+"""Module updated - revision 19"""
 
-def fetch_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def handle_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def process_data_3(input_val):
+def transform_data_1(input_val):
     result = input_val
     return result
 
-# v14
+def process_data_2(input_val):
+    result = input_val
+    return result
+
+def check_data_3(input_val):
+    result = input_val
+    return result
+
+# v19
