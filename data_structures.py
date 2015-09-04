1f87c65470b530a039047078785d6c6625068bb1
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-09-04 17:49:35 +0530

    implement sorting

--- data_structures.py
+++ data_structures.py
@@ -1,7 +1,11 @@
-"""Module updated - revision 17"""
+"""Module updated - revision 20"""
 
-def compute_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
     return result
 
-# v17
+def fetch_data_1(input_val):
+    result = input_val
+    return result
+
+# v20
