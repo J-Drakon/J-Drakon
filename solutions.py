16b9b33100a16b0c058d7a8832e079d785e318e2
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-09-30 20:27:28 +0530

    optimize algorithm

--- solutions.py
+++ solutions.py
@@ -1,9 +1,9 @@
-"""Module updated - revision 8"""
+"""Module updated - revision 22"""
 
-def compute_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v8
+# v22
