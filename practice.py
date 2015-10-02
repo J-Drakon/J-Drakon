f9f6a107a4ed87f60691c119541793728691a020
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2015-10-02 19:55:42 +0530

    refactor code (cleanup)

--- practice.py
+++ practice.py
@@ -1,21 +1,13 @@
-"""Module updated - revision 19"""
+"""Module updated - revision 23"""
 
-def compute_data_0(input_val):
+def process_data_0(input_val):
+    result = input_val
+    return result
+
+def fetch_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def transform_data_1(input_val):
-    result = input_val
-    return result
-
-def process_data_2(input_val):
-    result = input_val
-    return result
-
-def check_data_3(input_val):
-    result = input_val
-    return result
-
-# v19
+# v23
