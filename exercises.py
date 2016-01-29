6a310a668e4603e6f90a0755d4859a1ba2cacb4f
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-01-29 11:45:46 +0530

    optimize algorithm (for release)

--- exercises.py
+++ exercises.py
@@ -1,13 +1,15 @@
-"""Module updated - revision 28"""
+"""Module updated - revision 3"""
 
-def parse_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def check_data_1(input_val):
+def fetch_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v28
+# v3
