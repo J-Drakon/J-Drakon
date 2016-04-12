19b7efa21db34e2523bc413effff1d22594c6f91
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-04-12 19:01:23 +0530

    add test cases

--- practice.py
+++ practice.py
@@ -1,9 +1,21 @@
-"""Module updated - revision 4"""
+"""Module updated - revision 7"""
 
-def parse_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v4
+def fetch_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def transform_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v7
