c92f296c1a32c57c5ce5640f2109c6c78578ca6d
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-05-03 20:16:12 +0530

    practice recursion (wip)

--- exercises.py
+++ exercises.py
@@ -1,11 +1,21 @@
-"""Module updated - revision 5"""
+"""Module updated - revision 8"""
 
-def validate_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
-def check_data_1(input_val):
+def validate_data_1(input_val):
     result = input_val
     return result
 
-# v5
+def fetch_data_2(input_val):
+    result = input_val
+    return result
+
+def parse_data_3(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v8
