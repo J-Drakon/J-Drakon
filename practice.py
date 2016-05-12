ee53bdbef98f43c716446f22bb9fc98af88662d2
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-05-12 09:20:59 +0530

    add solution

--- practice.py
+++ practice.py
@@ -1,21 +1,11 @@
-"""Module updated - revision 7"""
+"""Module updated - revision 10"""
 
-def fetch_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def fetch_data_1(input_val):
+def transform_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def transform_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v7
+# v10
