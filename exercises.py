b6110a98dd2f18e4f74c190f434f35d77e3387d4
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-07-27 15:49:26 +0530

    add helper function

--- exercises.py
+++ exercises.py
@@ -1,21 +1,21 @@
-"""Module updated - revision 8"""
+"""Module updated - revision 12"""
 
-def check_data_0(input_val):
-    result = input_val
-    return result
-
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-def fetch_data_2(input_val):
-    result = input_val
-    return result
-
-def parse_data_3(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v8
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def transform_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v12
