40ac36e683c08210582b040743338ba59f87241d
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-08-06 17:37:37 +0530

    add helper function

--- solutions.py
+++ solutions.py
@@ -1,21 +1,13 @@
-"""Module updated - revision 5"""
+"""Module updated - revision 13"""
 
-def process_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
-def handle_data_1(input_val):
+def fetch_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def transform_data_2(input_val):
-    result = input_val
-    return result
-
-def fetch_data_3(input_val):
-    result = input_val
-    return result
-
-# v5
+# v13
