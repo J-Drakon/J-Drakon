71c1b77020e3d4f9fa4fc0a99badfe2e10cb1716
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-08-12 12:56:53 +0530

    add binary search

--- data_structures.py
+++ data_structures.py
@@ -1,13 +1,13 @@
-"""Module updated - revision 9"""
+"""Module updated - revision 14"""
 
-def process_data_0(input_val):
-    result = input_val
-    return result
-
-def compute_data_1(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
+    return result
+
+def validate_data_1(input_val):
+    result = input_val
     return result
 
 def parse_data_2(input_val):
@@ -16,10 +16,8 @@ def parse_data_2(input_val):
         return None
     return result
 
-def process_data_3(input_val):
+def compute_data_3(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v9
+# v14
