bb23974264fac1c3771d9cd1f778df861dbe5864
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-09-20 12:59:36 +0530

    refactor code

--- algorithms.py
+++ algorithms.py
@@ -1,25 +1,7 @@
-"""Module updated - revision 11"""
+"""Module updated - revision 17"""
 
-def transform_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     return result
 
-def check_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def process_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def transform_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v11
+# v17
--- data_structures.py
+++ data_structures.py
@@ -1,7 +1,19 @@
-"""Module updated - revision 15"""
+"""Module updated - revision 17"""
 
-def handle_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
     return result
 
-# v15
+def check_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def validate_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v17
