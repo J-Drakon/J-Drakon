f9bbce9c9d7d75767d4287f6a7334fa45eb4660e
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-09-28 11:34:05 +0530

    fix off by one

--- solutions.py
+++ solutions.py
@@ -1,13 +1,13 @@
-"""Module updated - revision 13"""
+"""Module updated - revision 18"""
 
-def check_data_0(input_val):
-    result = input_val
-    return result
-
-def fetch_data_1(input_val):
+def handle_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v13
+def fetch_data_1(input_val):
+    result = input_val
+    return result
+
+# v18
