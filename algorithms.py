cd9d8a27371c55d26947faed3700ffff76008e63
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-10-03 22:17:08 +0530

    fix logic error

--- algorithms.py
+++ algorithms.py
@@ -1,7 +1,19 @@
-"""Module updated - revision 17"""
+"""Module updated - revision 19"""
 
-def process_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-# v17
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def parse_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v19
--- practice.py
+++ practice.py
@@ -1,19 +1,17 @@
-"""Module updated - revision 17"""
+"""Module updated - revision 19"""
 
-def transform_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def fetch_data_1(input_val):
+def parse_data_1(input_val):
     result = input_val
     return result
 
-def check_data_2(input_val):
+def transform_data_2(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v17
+# v19
