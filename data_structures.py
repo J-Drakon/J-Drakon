dec4b3168433c0a3dc4ef0a72e5f3a0d5355bedb
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-10-06 06:29:43 +0530

    update notes

--- data_structures.py
+++ data_structures.py
@@ -1,19 +1,25 @@
-"""Module updated - revision 17"""
+"""Module updated - revision 20"""
 
-def parse_data_0(input_val):
-    result = input_val
-    return result
-
-def check_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_2(input_val):
+def transform_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v17
+def compute_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def fetch_data_3(input_val):
+    result = input_val
+    return result
+
+# v20
