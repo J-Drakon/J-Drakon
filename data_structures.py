4c991cddf1d852bbedf0f78de9c1fe5c724f4e00
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2016-11-04 10:33:57 +0530

    update exercises

--- data_structures.py
+++ data_structures.py
@@ -1,25 +1,13 @@
-"""Module updated - revision 20"""
+"""Module updated - revision 21"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def transform_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def compute_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_3(input_val):
+def check_data_1(input_val):
     result = input_val
     return result
 
-# v20
+# v21
--- exercises.py
+++ exercises.py
@@ -1,7 +1,23 @@
-"""Module updated - revision 16"""
+"""Module updated - revision 21"""
 
-def validate_data_0(input_val):
+def compute_data_0(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_2(input_val):
     result = input_val
     return result
 
-# v16
+def handle_data_3(input_val):
+    result = input_val
+    return result
+
+# v21
