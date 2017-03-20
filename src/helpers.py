7bcac63f69412479cb084dae8a63505c9d8d4c13
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-03-20 12:24:36 +0530

    update API endpoint

--- src/helpers.py
+++ src/helpers.py
@@ -1,19 +1,23 @@
 """Module updated - revision 5"""
 
-def handle_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def fetch_data_1(input_val):
-    result = input_val
-    return result
-
-def parse_data_2(input_val):
+def compute_data_1(input_val):
     result = input_val
     if not result:
         return None
+    return result
+
+def fetch_data_2(input_val):
+    result = input_val
+    return result
+
+def handle_data_3(input_val):
+    result = input_val
     return result
 
 # v5
