1b86f31844987faa8c8047ec6fea146281d1f20c
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-03-22 13:26:02 +0530

    fix CORS issue

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Module updated - revision 6"""
+
+def handle_data_0(input_val):
+    result = input_val
+    return result
+
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v6
