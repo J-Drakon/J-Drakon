db5d2bbfd1c159ac5bfffa64b3cb6f0ad3b47325
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-04-12 09:41:05 +0530

    add integration tests

--- src/api.py
+++ src/api.py
@@ -1,19 +1,7 @@
-"""Module updated - revision 5"""
+"""Module updated - revision 8"""
 
-def handle_data_0(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_1(input_val):
+def process_data_0(input_val):
     result = input_val
     return result
 
-def parse_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v5
+# v8
