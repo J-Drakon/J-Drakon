3bd3ce6768cdd174b2e5e549f6dd5bb2d28b0c6e
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-05-08 10:56:09 +0530

    code cleanup

--- src/helpers.py
+++ src/helpers.py
@@ -1,23 +1,19 @@
-"""Module updated - revision 5"""
+"""Module updated - revision 13"""
 
-def validate_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_1(input_val):
+def transform_data_1(input_val):
+    result = input_val
+    return result
+
+def validate_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def fetch_data_2(input_val):
-    result = input_val
-    return result
-
-def handle_data_3(input_val):
-    result = input_val
-    return result
-
-# v5
+# v13
