f37645d16ff55d006a586fc607edcd2131b092ec
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-05-21 09:46:34 +0530

    improve performance

--- src/api.py
+++ src/api.py
@@ -1,7 +1,9 @@
-"""Module updated - revision 8"""
+"""Module updated - revision 15"""
 
-def process_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v8
+# v15
