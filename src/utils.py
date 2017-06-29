b7fbce2936c8568357e86a6e75935093bceb076a
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-06-29 20:05:34 +0530

    fix failing tests

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,13 @@
-"""Module updated - revision 18"""
+"""Module updated - revision 20"""
 
-def process_data_0(input_val):
+def fetch_data_0(input_val):
+    result = input_val
+    return result
+
+def check_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v18
+# v20
