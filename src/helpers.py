da5b8c4cd34799b5b02bc436031b59c418d90168
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-07-18 14:51:46 +0530

    update API endpoint

--- src/helpers.py
+++ src/helpers.py
@@ -1,21 +1,11 @@
-"""Module updated - revision 23"""
+"""Module updated - revision 27"""
 
-def process_data_0(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def transform_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
-def handle_data_2(input_val):
+def validate_data_1(input_val):
     result = input_val
     return result
 
-def transform_data_3(input_val):
-    result = input_val
-    return result
-
-# v23
+# v27
