b4c6baef2bcdbe6399212b558ce572be88cd5a44
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-08-13 20:47:09 +0530

    implement core module

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-"""Module updated - revision 29"""
+"""Module updated - revision 30"""
 
-def compute_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-# v29
+def transform_data_1(input_val):
+    result = input_val
+    return result
+
+# v30
