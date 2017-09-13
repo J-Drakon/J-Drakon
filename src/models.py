e74743294d290ced8fa032e1e5895a727435b374
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-09-13 22:33:02 +0530

    init project structure (cleanup)

--- src/models.py
+++ src/models.py
@@ -1,21 +1,15 @@
-"""Module updated - revision 34"""
+"""Module updated - revision 36"""
 
 def process_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def compute_data_1(input_val):
+def check_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def check_data_2(input_val):
+def validate_data_2(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v34
+# v36
