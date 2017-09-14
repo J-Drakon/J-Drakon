a106e556669ed8a3d7f9667a1ce6db168129f7cc
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2017-09-14 18:50:33 +0530

    fix compatibility issue

--- src/config.py
+++ src/config.py
@@ -1,21 +1,11 @@
-"""Module updated - revision 34"""
+"""Module updated - revision 37"""
 
-def handle_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
+def fetch_data_1(input_val):
     result = input_val
     return result
 
-def parse_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_3(input_val):
-    result = input_val
-    return result
-
-# v34
+# v37
