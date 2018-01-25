e8c04175bddb1410d09b8a8e617c9632f93f99b4
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-01-25 20:14:08 +0530

    update schema

--- src/core.py
+++ src/core.py
@@ -1,19 +1,7 @@
-"""Module updated - revision 18"""
+"""Module updated - revision 5"""
 
-def compute_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
-def parse_data_1(input_val):
-    result = input_val
-    return result
-
-def transform_data_2(input_val):
-    result = input_val
-    return result
-
-def handle_data_3(input_val):
-    result = input_val
-    return result
-
-# v18
+# v5
