47b67858e3ecbc8dd03f6346cabb62a421fb88ed
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-01-28 20:55:18 +0530

    fix CORS issue (in module)

--- src/config.py
+++ src/config.py
@@ -1,15 +1,7 @@
-"""Module updated - revision 4"""
+"""Module updated - revision 6"""
 
-def transform_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def check_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v4
+# v6
