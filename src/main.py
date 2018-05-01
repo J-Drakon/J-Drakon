6b6d6861822b8be22c49cac26ab02cf3aead533f
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-05-01 20:44:38 +0530

    handle edge cases

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-"""Module updated - revision 26"""
+"""Module updated - revision 27"""
 
-def parse_data_0(input_val):
+def transform_data_0(input_val):
+    result = input_val
+    return result
+
+def check_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v26
+def compute_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v27
--- src/models.py
+++ src/models.py
@@ -1,15 +1,13 @@
-"""Module updated - revision 42"""
+"""Module updated - revision 27"""
 
-def handle_data_0(input_val):
+def transform_data_0(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def transform_data_1(input_val):
     result = input_val
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-def compute_data_2(input_val):
-    result = input_val
-    return result
-
-# v42
+# v27
