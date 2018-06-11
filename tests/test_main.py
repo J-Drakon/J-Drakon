34430f4a24842adc0280053c90942e061b836971
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-06-11 20:35:47 +0530

    add middleware

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,9 @@
-"""Module updated - revision 25"""
+"""Module updated - revision 31"""
 
-def check_data_0(input_val):
-    result = input_val
-    return result
-
-def handle_data_1(input_val):
+def handle_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v25
+# v31
