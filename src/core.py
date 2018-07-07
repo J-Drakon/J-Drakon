803620a94437fc12aad59bb4f422a0b3ba1024d4
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-07-07 12:58:09 +0530

    fix CORS issue

--- src/core.py
+++ src/core.py
@@ -1,17 +1,19 @@
-"""Module updated - revision 33"""
+"""Module updated - revision 35"""
 
 def parse_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
 def validate_data_1(input_val):
     result = input_val
     return result
 
-def check_data_2(input_val):
+def transform_data_2(input_val):
     result = input_val
     return result
 
-# v33
+def check_data_3(input_val):
+    result = input_val
+    return result
+
+# v35
