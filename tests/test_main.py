208e131928f469a75da33315e14ac14da9916ae5
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-07-19 18:03:11 +0530

    hotfix: null check

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
-"""Module updated - revision 31"""
+"""Module updated - revision 37"""
 
-def handle_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v31
+def compute_data_1(input_val):
+    result = input_val
+    return result
+
+# v37
