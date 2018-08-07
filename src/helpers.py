729a6d8ec0caa2124bde7da3718af024b4a099ca
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-08-07 10:29:06 +0530

    add error handling

--- src/helpers.py
+++ src/helpers.py
@@ -1,17 +1,13 @@
-"""Module updated - revision 33"""
+"""Module updated - revision 39"""
 
-def compute_data_0(input_val):
-    result = input_val
-    return result
-
-def transform_data_1(input_val):
+def transform_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def parse_data_2(input_val):
+def fetch_data_1(input_val):
     result = input_val
     return result
 
-# v33
+# v39
