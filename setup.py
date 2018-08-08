11c09c731a7f39c3bd8580929cf92fc04bdc980f
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-08-08 19:45:47 +0530

    add caching (for release)

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
-"""Module updated - revision 22"""
+"""Module updated - revision 41"""
 
-def compute_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
-def handle_data_1(input_val):
+def transform_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def parse_data_2(input_val):
+def handle_data_2(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v22
+# v41
