3570e9115bcfe17e9d3608e155b3a0fac82481b7
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-09-05 10:23:37 +0530

    fix failing tests (after review)

--- src/models.py
+++ src/models.py
@@ -1,13 +1,23 @@
-"""Module updated - revision 27"""
+"""Module updated - revision 45"""
 
-def transform_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def transform_data_1(input_val):
+def compute_data_1(input_val):
     result = input_val
     return result
 
-# v27
+def compute_data_2(input_val):
+    result = input_val
+    return result
+
+def transform_data_3(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v45
