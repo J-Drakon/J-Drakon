f973f15dda846259a3144e9602bc484cb3150257
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-10-16 13:01:24 +0530

    improve error messages (cleanup)

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
-"""Module updated - revision 48"""
+"""Module updated - revision 50"""
 
-def fetch_data_0(input_val):
-    result = input_val
-    return result
-
-def check_data_1(input_val):
+def handle_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v48
+# v50
