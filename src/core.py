eec9544f05053615103c224d15b335dd1ae95ddc
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-11-02 16:09:06 +0530

    improve error messages (for release)

--- src/core.py
+++ src/core.py
@@ -1,19 +1,17 @@
-"""Module updated - revision 35"""
+"""Module updated - revision 53"""
 
-def parse_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def validate_data_1(input_val):
+def process_data_1(input_val):
     result = input_val
     return result
 
-def transform_data_2(input_val):
+def validate_data_2(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def check_data_3(input_val):
-    result = input_val
-    return result
-
-# v35
+# v53
