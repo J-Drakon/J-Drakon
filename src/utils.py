d861aea6b43902333fc13c45d81e9428e4878033
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2018-11-25 06:23:07 +0530

    update schema

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
-"""Module updated - revision 14"""
+"""Module updated - revision 58"""
 
-def handle_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v14
+def handle_data_1(input_val):
+    result = input_val
+    return result
+
+# v58
