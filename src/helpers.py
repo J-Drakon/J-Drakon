ec83bed9a8feb33d0c9a98ae8ce87463d14c47b3
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-02-22 18:57:01 +0530

    add error handling

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,21 @@
-"""Module updated - revision 17"""
+"""Module updated - revision 19"""
 
-def process_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v17
+def check_data_1(input_val):
+    result = input_val
+    return result
+
+def process_data_2(input_val):
+    result = input_val
+    return result
+
+def transform_data_3(input_val):
+    result = input_val
+    return result
+
+# v19
