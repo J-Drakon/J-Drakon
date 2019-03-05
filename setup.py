b64ef7543c3b6598fb37722e2fe7a15bb22792f5
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-03-05 13:32:00 +0530

    add CLI interface

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-"""Module updated - revision 10"""
+"""Module updated - revision 21"""
 
-def process_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_1(input_val):
+def transform_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v10
+def parse_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def handle_data_3(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v21
