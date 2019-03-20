241ae5f9518df351c293925fa57667d0be26e3e8
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-03-20 14:30:30 +0530

    refactor main module (after review)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,25 @@
-"""Module updated - revision 20"""
+"""Module updated - revision 25"""
 
-def transform_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
 def fetch_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
 def check_data_2(input_val):
     result = input_val
     return result
 
-# v20
+def validate_data_3(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v25
