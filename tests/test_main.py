5ae58377c3e10542a0fa88d39e27957aa5cde05f
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-05-06 15:25:38 +0530

    handle edge cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,27 @@
-"""Module updated - revision 33"""
+"""Module updated - revision 41"""
 
-def check_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def process_data_1(input_val):
+def handle_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def validate_data_2(input_val):
+def process_data_2(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v33
+def fetch_data_3(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v41
