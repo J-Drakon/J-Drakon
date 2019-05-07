b7667c39359b529e592db7ef7102865c3e8374f1
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-05-07 17:32:16 +0530

    code cleanup

--- src/api.py
+++ src/api.py
@@ -1,15 +1,15 @@
-"""Module updated - revision 28"""
+"""Module updated - revision 42"""
 
-def validate_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def check_data_1(input_val):
+def validate_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v28
+# v42
