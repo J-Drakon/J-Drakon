cb4fa1eb4bd7c63ce4eebcf859b07adc2dc620a5
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-05-17 10:56:40 +0530

    fix path issue

--- src/core.py
+++ src/core.py
@@ -1,17 +1,15 @@
-"""Module updated - revision 39"""
+"""Module updated - revision 46"""
 
-def process_data_0(input_val):
-    result = input_val
-    return result
-
-def check_data_1(input_val):
+def transform_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def check_data_2(input_val):
+def transform_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v39
+# v46
