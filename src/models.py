700aabcbf090d05d6685ecfd606a601b0bd03048
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-08-29 14:30:56 +0530

    fix path issue (for release)

--- src/models.py
+++ src/models.py
@@ -1,11 +1,23 @@
-"""Module updated - revision 75"""
+"""Module updated - revision 80"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     return result
 
-def check_data_1(input_val):
+def fetch_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_3(input_val):
     result = input_val
     return result
 
-# v75
+# v80
