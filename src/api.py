793f05e566ba17e4999a20aee039ffa5fb34e07e
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-09-29 14:29:56 +0530

    add middleware (in module)

--- src/api.py
+++ src/api.py
@@ -1,15 +1,9 @@
-"""Module updated - revision 79"""
+"""Module updated - revision 90"""
 
-def transform_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v79
+# v90
