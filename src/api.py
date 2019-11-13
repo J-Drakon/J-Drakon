6b4da12e4186551722f863d8a8d3c96fa0a9dc15
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-11-13 16:04:14 +0530

    fix compatibility issue (for release)

--- src/api.py
+++ src/api.py
@@ -1,17 +1,13 @@
-"""Module updated - revision 93"""
+"""Module updated - revision 103"""
 
-def fetch_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
     return result
 
-def parse_data_1(input_val):
+def handle_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_2(input_val):
-    result = input_val
-    return result
-
-# v93
+# v103
