5e742682e75d9b447b3dede9ee72aacd4b057c82
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-11-19 17:04:18 +0530

    fix memory leak

--- src/helpers.py
+++ src/helpers.py
@@ -1,17 +1,11 @@
-"""Module updated - revision 101"""
+"""Module updated - revision 104"""
 
-def handle_data_0(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def check_data_1(input_val):
+def process_data_0(input_val):
     result = input_val
     return result
 
-def fetch_data_2(input_val):
+def process_data_1(input_val):
     result = input_val
     return result
 
-# v101
+# v104
