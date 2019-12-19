bacbfcac60ea03afa68e46282a7170c68276a147
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2019-12-19 08:34:34 +0530

    handle edge cases

--- src/api.py
+++ src/api.py
@@ -1,13 +1,21 @@
-"""Module updated - revision 103"""
+"""Module updated - revision 111"""
 
-def check_data_0(input_val):
-    result = input_val
-    return result
-
-def handle_data_1(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v103
+def validate_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def handle_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v111
