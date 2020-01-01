2efdaeae872300675aae6b5770d04b00f3e282bb
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-01-01 20:04:24 +0530

    update README with usage (for release)

--- src/api.py
+++ src/api.py
@@ -1,21 +1,17 @@
-"""Module updated - revision 111"""
+"""Module updated - revision 1"""
 
-def compute_data_0(input_val):
+def fetch_data_0(input_val):
+    result = input_val
+    return result
+
+def check_data_1(input_val):
+    result = input_val
+    return result
+
+def validate_data_2(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def handle_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v111
+# v1
