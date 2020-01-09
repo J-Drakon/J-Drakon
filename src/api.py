a57b7427f119e7f8a4d42c3d6760f5036ad85668
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-01-09 18:26:13 +0530

    improve logging (cleanup)

--- src/api.py
+++ src/api.py
@@ -1,4 +1,4 @@
-"""Module updated - revision 1"""
+"""Module updated - revision 4"""
 
 def fetch_data_0(input_val):
     result = input_val
@@ -6,6 +6,8 @@ def fetch_data_0(input_val):
 
 def check_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
 def validate_data_2(input_val):
@@ -14,4 +16,4 @@ def validate_data_2(input_val):
         return None
     return result
 
-# v1
+# v4
