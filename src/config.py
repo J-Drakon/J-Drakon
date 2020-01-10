e5700d9ebcf5077fa2b9572fe0fd5071615019bb
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-01-10 09:03:57 +0530

    add env config (cleanup)

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
-"""Module updated - revision 3"""
+"""Module updated - revision 5"""
 
-def transform_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
 def fetch_data_1(input_val):
@@ -10,4 +12,4 @@ def fetch_data_1(input_val):
         return None
     return result
 
-# v3
+# v5
