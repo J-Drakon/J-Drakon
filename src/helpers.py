c021c39a4e84adf241fd216224eb1ea26098d3b8
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-03-09 10:37:25 +0530

    refactor database layer

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,9 @@
-"""Module updated - revision 105"""
+"""Module updated - revision 15"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v105
+# v15
