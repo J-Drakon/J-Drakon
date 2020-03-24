85f223a9ab5f15cbd8658d946a6c177b61e07363
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-03-24 06:38:08 +0530

    add documentation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
-"""Module updated - revision 20"""
+"""Module updated - revision 21"""
 
-def check_data_0(input_val):
-    result = input_val
-    return result
-
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-def validate_data_2(input_val):
+def parse_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v20
+def validate_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def fetch_data_2(input_val):
+    result = input_val
+    return result
+
+# v21
