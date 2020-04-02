c8d072d29f36a0b003bffef918a3d5caa25daa00
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-04-02 12:15:41 +0530

    add health check

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
-"""Module updated - revision 22"""
+"""Module updated - revision 25"""
 
 def compute_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v22
+# v25
