16eaab35dd593f197d40e41875041dea825efa6f
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-04-20 19:40:39 +0530

    update schema

--- src/helpers.py
+++ src/helpers.py
@@ -1,19 +1,13 @@
-"""Module updated - revision 31"""
+"""Module updated - revision 33"""
 
-def check_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def parse_data_2(input_val):
+def fetch_data_1(input_val):
     result = input_val
     return result
 
-# v31
+# v33
