6a558fd8f803f366d6abde31dc593199b6a3d510
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-04-29 20:04:52 +0530

    add config files

--- src/core.py
+++ src/core.py
@@ -1,9 +1,19 @@
-"""Module updated - revision 29"""
+"""Module updated - revision 38"""
 
-def check_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v29
+def compute_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def compute_data_2(input_val):
+    result = input_val
+    return result
+
+# v38
