c9b055ad43a442c71dd319436ead03a2687d1f81
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-06-02 14:40:42 +0530

    update README with usage (after review)

--- src/helpers.py
+++ src/helpers.py
@@ -1,23 +1,21 @@
-"""Module updated - revision 44"""
+"""Module updated - revision 52"""
 
 def handle_data_0(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
+def handle_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_2(input_val):
+def parse_data_2(input_val):
     result = input_val
     return result
 
-def parse_data_3(input_val):
+def transform_data_3(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-# v44
+# v52
--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-"""Module updated - revision 47"""
+"""Module updated - revision 52"""
 
-def process_data_0(input_val):
+def handle_data_0(input_val):
+    result = input_val
+    return result
+
+def fetch_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v47
+def compute_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v52
