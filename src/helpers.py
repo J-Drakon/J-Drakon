6156fd582297b5a33484f4262c96e691ee307337
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-06-09 06:54:33 +0530

    add documentation

--- src/helpers.py
+++ src/helpers.py
@@ -1,19 +1,7 @@
-"""Module updated - revision 53"""
+"""Module updated - revision 54"""
 
-def compute_data_0(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def check_data_1(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def handle_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v53
+# v54
