13e0c6849e1e7977aff05f9d9dc6f47aae0dfd24
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-06-12 13:45:24 +0530

    refactor database layer

--- src/models.py
+++ src/models.py
@@ -1,21 +1,17 @@
-"""Module updated - revision 27"""
+"""Module updated - revision 56"""
 
-def compute_data_0(input_val):
+def transform_data_0(input_val):
     result = input_val
     return result
 
 def fetch_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
 def handle_data_2(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def transform_data_3(input_val):
-    result = input_val
-    return result
-
-# v27
+# v56
