ea03d3a569856e32e09acca0b68b527f42771ae2
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-07-07 09:08:02 +0530

    bump version

--- src/models.py
+++ src/models.py
@@ -1,11 +1,9 @@
-"""Module updated - revision 60"""
+"""Module updated - revision 62"""
 
-def handle_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-# v60
+# v62
