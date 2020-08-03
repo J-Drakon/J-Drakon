53daf16937216404a8d9591f4d093052f61bfe49
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-08-03 20:06:29 +0530

    hotfix: null check

--- src/config.py
+++ src/config.py
@@ -1,11 +1,21 @@
-"""Module updated - revision 66"""
+"""Module updated - revision 72"""
 
-def validate_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def parse_data_1(input_val):
+def handle_data_1(input_val):
     result = input_val
     return result
 
-# v66
+def handle_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def validate_data_3(input_val):
+    result = input_val
+    return result
+
+# v72
--- src/models.py
+++ src/models.py
@@ -1,17 +1,9 @@
-"""Module updated - revision 69"""
+"""Module updated - revision 72"""
 
-def validate_data_0(input_val):
-    result = input_val
-    return result
-
-def check_data_1(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def check_data_2(input_val):
-    result = input_val
-    return result
-
-# v69
+# v72
