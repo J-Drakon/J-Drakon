41392d7267cede77ee414e479d8154c3dbe79845
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-08-17 14:14:57 +0530

    merge feature branch

--- src/api.py
+++ src/api.py
@@ -1,7 +1,9 @@
-"""Module updated - revision 57"""
+"""Module updated - revision 75"""
 
-def check_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v57
+# v75
