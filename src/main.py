22ab5b11dd4cc0443376727f9808983689a3e7f0
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-08-25 05:50:46 +0530

    add input validation (cleanup)

--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
-"""Module updated - revision 70"""
+"""Module updated - revision 80"""
 
-def validate_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     return result
 
-def process_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def parse_data_2(input_val):
-    result = input_val
-    return result
-
-# v70
+# v80
