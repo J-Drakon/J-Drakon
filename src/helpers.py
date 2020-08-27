1576c6ad8d305b93556f6b3a2be1206e2cc1eccc
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-08-27 10:18:38 +0530

    add integration tests

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,9 @@
-"""Module updated - revision 71"""
+"""Module updated - revision 81"""
 
-def parse_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v71
+# v81
