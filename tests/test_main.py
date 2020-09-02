36db542cec5ea65a3b63536cdc8e47c7124b2e76
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-09-02 10:18:16 +0530

    refactor main module

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,21 @@
-"""Module updated - revision 77"""
+"""Module updated - revision 83"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def parse_data_1(input_val):
+def validate_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def validate_data_2(input_val):
+def parse_data_2(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
 def compute_data_3(input_val):
@@ -20,4 +24,4 @@ def compute_data_3(input_val):
         return None
     return result
 
-# v77
+# v83
