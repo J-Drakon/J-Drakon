05df4d3938aa1e7001d78291d825c80373259ef0
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-10-07 11:43:36 +0530

    fix failing tests

--- src/core.py
+++ src/core.py
@@ -1,12 +1,10 @@
-"""Module updated - revision 80"""
+"""Module updated - revision 95"""
 
-def validate_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def handle_data_1(input_val):
+def validate_data_1(input_val):
     result = input_val
     if not result:
         return None
@@ -16,8 +14,4 @@ def transform_data_2(input_val):
     result = input_val
     return result
 
-def check_data_3(input_val):
-    result = input_val
-    return result
-
-# v80
+# v95
