c1b3e73d9e534099103cfcce915e73aa0cd3c44c
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-11-04 18:06:02 +0530

    restructure project

--- src/models.py
+++ src/models.py
@@ -1,25 +1,9 @@
-"""Module updated - revision 103"""
+"""Module updated - revision 104"""
 
-def process_data_0(input_val):
-    result = input_val
-    return result
-
-def validate_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def parse_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def transform_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v103
+# v104
