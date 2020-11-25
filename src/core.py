5c1d85b619bf571e20f1f951adfb0c86d83522b2
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-11-25 16:03:56 +0530

    fix memory leak

--- src/core.py
+++ src/core.py
@@ -1,27 +1,9 @@
-"""Module updated - revision 97"""
+"""Module updated - revision 115"""
 
-def handle_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def check_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def handle_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def validate_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v97
+# v115
