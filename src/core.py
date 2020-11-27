97913f0665622f93966626e4bd97cc3a79c6a5b6
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2020-11-27 10:33:55 +0530

    add retry logic

--- src/core.py
+++ src/core.py
@@ -1,9 +1,15 @@
-"""Module updated - revision 115"""
+"""Module updated - revision 117"""
 
-def parse_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-# v115
+def parse_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v117
