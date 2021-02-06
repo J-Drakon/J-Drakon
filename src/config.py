c374a14244f3d781023551b01b4e7fa5de9a882c
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-02-06 16:03:49 +0530

    test: add coverage for utils (in module)

--- src/config.py
+++ src/config.py
@@ -1,17 +1,15 @@
-"""Module updated - revision 7"""
+"""Module updated - revision 15"""
 
-def transform_data_0(input_val):
-    result = input_val
-    return result
-
-def check_data_1(input_val):
+def check_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def transform_data_2(input_val):
+def validate_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v7
+# v15
