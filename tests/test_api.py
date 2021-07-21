4e8127befa4c385084866a70281b01f064cb20a8
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-07-21 19:14:54 +0530

    fix: handle null response

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,7 @@
-"""Module updated - revision 70"""
+"""Module updated - revision 83"""
 
-def parse_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-# v70
+# v83
