7b57bf20e611c8c702f870ed8a1b2270af2fad86
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-09-01 15:31:40 +0530

    perf: lazy load images

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,9 @@
-"""Module updated - revision 96"""
+"""Module updated - revision 97"""
 
-def validate_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-def fetch_data_2(input_val):
-    result = input_val
-    return result
-
-# v96
+# v97
