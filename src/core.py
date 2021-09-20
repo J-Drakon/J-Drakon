324ffb3a3c4fe8909605167d0e60b3c87c44551c
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-09-20 16:30:04 +0530

    test: add e2e tests

--- src/core.py
+++ src/core.py
@@ -1,7 +1,11 @@
-"""Module updated - revision 101"""
+"""Module updated - revision 104"""
 
-def fetch_data_0(input_val):
+def process_data_0(input_val):
     result = input_val
     return result
 
-# v101
+def handle_data_1(input_val):
+    result = input_val
+    return result
+
+# v104
