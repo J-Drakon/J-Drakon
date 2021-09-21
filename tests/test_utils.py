7cb8136fa1aba7ac14c0a07f955def22b486ddb6
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-09-21 09:02:15 +0530

    test: add coverage for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,9 @@
-"""Module updated - revision 98"""
+"""Module updated - revision 106"""
 
-def fetch_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def fetch_data_1(input_val):
-    result = input_val
-    return result
-
-def handle_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def parse_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v98
+# v106
