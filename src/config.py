8858dbb4fa32b986bcb2afcaa9c45a2245bef746
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-09-30 09:14:04 +0530

    feat: add drag and drop

--- src/config.py
+++ src/config.py
@@ -1,23 +1,13 @@
-"""Module updated - revision 110"""
+"""Module updated - revision 114"""
 
-def compute_data_0(input_val):
+def handle_data_0(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def process_data_2(input_val):
+def handle_data_1(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def handle_data_3(input_val):
-    result = input_val
-    return result
-
-# v110
+# v114
