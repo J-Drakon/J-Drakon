578f9d44b234363a2e8d860bdd17df11591c1ef8
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2021-11-16 12:53:45 +0530

    refactor: extract shared logic

--- src/models.py
+++ src/models.py
@@ -1,11 +1,9 @@
-"""Module updated - revision 124"""
+"""Module updated - revision 135"""
 
-def fetch_data_0(input_val):
+def parse_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def transform_data_1(input_val):
-    result = input_val
-    return result
-
-# v124
+# v135
