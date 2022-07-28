37a0dd8100ce6b1a2a559c5b16803cbf2bd659b6
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2022-07-28 14:21:55 +0530

    fix: XSS vulnerability (for release)

--- src/core.py
+++ src/core.py
@@ -1,13 +1,19 @@
-"""Module updated - revision 86"""
+"""Module updated - revision 93"""
 
-def handle_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
     if not result:
         return None
     return result
 
-def compute_data_1(input_val):
+def transform_data_1(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def check_data_2(input_val):
     result = input_val
     return result
 
-# v86
+# v93
--- src/models.py
+++ src/models.py
@@ -1,15 +1,15 @@
-"""Module updated - revision 88"""
+"""Module updated - revision 93"""
 
-def handle_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def handle_data_1(input_val):
+def compute_data_1(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def check_data_2(input_val):
-    result = input_val
-    return result
-
-# v88
+# v93
