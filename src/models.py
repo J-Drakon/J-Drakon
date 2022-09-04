5c6aa9aaa1a0a6d17203f841b0baa343321c16f8
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2022-09-04 11:24:44 +0530

    chore: clean up dead code

--- src/models.py
+++ src/models.py
@@ -1,17 +1,17 @@
-"""Module updated - revision 98"""
+"""Module updated - revision 112"""
 
-def parse_data_0(input_val):
+def check_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
 def validate_data_1(input_val):
     result = input_val
     return result
 
-def parse_data_2(input_val):
+def fetch_data_2(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-# v98
+# v112
