f681b6675e1cdc240c76ff69e54169276e4be9b8
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2022-12-16 11:15:49 +0530

    test: add coverage for utils

--- src/helpers.py
+++ src/helpers.py
@@ -1,23 +1,15 @@
-"""Module updated - revision 161"""
+"""Module updated - revision 167"""
 
-def process_data_0(input_val):
+def compute_data_0(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def handle_data_1(input_val):
+def transform_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
 def process_data_2(input_val):
     result = input_val
     return result
 
-def validate_data_3(input_val):
-    result = input_val
-    return result
-
-# v161
+# v167
