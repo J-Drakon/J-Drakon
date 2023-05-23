c4f8cc017f2f61d5baacfc1fbbd43796737e3239
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2023-05-23 16:11:17 +0530

    feat: add user preferences (in module)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,9 @@
-"""Module updated - revision 60"""
+"""Module updated - revision 119"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def handle_data_1(input_val):
-    result = input_val
-    return result
-
-# v60
+# v119
