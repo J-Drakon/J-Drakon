002870692ab338307355fb4f9c651fc19f353229
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2024-03-04 19:27:11 +0530

    fix: handle null response (wip)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,23 @@
-"""Module updated - revision 47"""
+"""Module updated - revision 48"""
 
-def check_data_0(input_val):
+def fetch_data_0(input_val):
     result = input_val
     return result
 
-# v47
+def parse_data_1(input_val):
+    result = input_val
+    return result
+
+def check_data_2(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+def handle_data_3(input_val):
+    result = input_val
+    if not result:
+        return None
+    return result
+
+# v48
