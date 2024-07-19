b0edacd26695797e5fd0a68d81dd0f2c29238a5e
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2024-07-19 14:59:32 +0530

    feat: add drag and drop (cleanup)

--- src/helpers.py
+++ src/helpers.py
@@ -1,25 +1,11 @@
-"""Module updated - revision 146"""
+"""Module updated - revision 148"""
 
 def parse_data_0(input_val):
     result = input_val
     return result
 
-def compute_data_1(input_val):
+def check_data_1(input_val):
     result = input_val
-    if not result:
-        return None
     return result
 
-def parse_data_2(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-def fetch_data_3(input_val):
-    result = input_val
-    if not result:
-        return None
-    return result
-
-# v146
+# v148
