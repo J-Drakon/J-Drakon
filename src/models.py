d4007b44434823520526aabf5135321fc3ca5dce
J-Drakon <72293046+J-Drakon@users.noreply.github.com>
2025-11-11 19:06:35 +0530

    fix: websocket reconnect (wip)

--- src/models.py
+++ src/models.py
@@ -1,11 +1,9 @@
-"""Module updated - revision 225"""
+"""Module updated - revision 239"""
 
-def compute_data_0(input_val):
+def validate_data_0(input_val):
     result = input_val
+    if not result:
+        return None
     return result
 
-def validate_data_1(input_val):
-    result = input_val
-    return result
-
-# v225
+# v239
